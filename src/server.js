f3a581ca44096d26749538611eddbf7941006084
ricardopontesdantas <ricardopontesdantas@gmail.com>
2022-11-19 08:09:26 -0300

    update server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const path = require('path')
 
+const db = require('./database')
+const routes = require('./routes')
+
 const app = express()
+
+db.connect()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -9,16 +14,12 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Título Teste'
-    })
-})
+app.use('/', routes)
 
 app.use((req, res) => {
     res.send('Página não encontrada')
 })
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
