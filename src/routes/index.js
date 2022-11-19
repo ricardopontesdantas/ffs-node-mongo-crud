50d99c06a8475173e556a4999a56f6a1a23fc378
ricardopontesdantas <ricardopontesdantas@gmail.com>
2022-11-19 08:08:58 -0300

    create routes file

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+
+const CustomersController = require('../controllers/customers')
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Título Teste'
+    })
+})
+
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Cadastro de Clientes'
+    })
+})
+
+router.post('/register/add', CustomersController.add)
+
+module.exports = router
